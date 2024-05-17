bdfd108bdedd4e2a0550248a075aabb38ac1d5a8
sunitamb <sunamb0444@gmail.com>
2024-05-17 14:28:08 +0530

    Indian Architect development

--- src/main/java/qaclickacademy/App.java
+++ src/main/java/qaclickacademy/App.java
@@ -10,5 +10,12 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println("sample");
+      //sgsh
+        System.out.println("sample1");
+        System.out.println("sample2");
+        System.out.println("sample3");
+        System.out.println("sample4");
+        
+        System.out.println("Indian Guy using develop branch");
     }
 }
